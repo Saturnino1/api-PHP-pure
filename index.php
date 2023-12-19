5395133f06baca75a992bb68fb841b664e53dee3
Saturnino1 <saturninomoreira2003@gmail.com>
2023-12-19 12:54:17 -0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    header("Access-Control-Allow-Origin: * ");
+    header("Access-Control-Allow-Headers: * ");
+    header("Content-type: aplication/json");
+
+    echo json_encode(["message" => "succesfull "]);
+
+    // include_once "Routes/index.route.php";
+
+
+
+
+
+
+
+
+
